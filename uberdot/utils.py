8ae32c7b2e48972e90065750bf93f3356d4d9b64
Erik Schulz <archlinuxuser@protonmail.com>
2020-02-11 00:13:43 +0100

    whatever

--- uberdot/utils.py
+++ uberdot/utils.py
@@ -657,24 +657,3 @@ def md5(string):
     if isinstance(string, str):
         string = string.encode()
     return hashlib.md5(string).hexdigest()
-
-
-def is_version_smaller(version_a, version_b):
-    match = re.search(r"(\d+)\.(\d+)\.(\d+)", version_a)
-    major_a, minor_a, patch_a = match.groups()
-    major_a, minor_a, patch_a = int(major_a), int(minor_a), int(patch_a)
-    major_b, minor_b, patch_b = match.groups()
-    major_b, minor_b, patch_b = int(major_b), int(minor_b), int(patch_b)
-    if major_a > major_b:
-        return False
-    if major_a < major_b:
-        return True
-    if minor_a > minor_b:
-        return False
-    if minor_a < minor_b:
-        return True
-    if patch_a > patch_b:
-        return False
-    if patch_a < patch_b:
-        return True
-    return False
